066251cfd35cc3ade14e55194eafcc14b6288c62
Anis Ahmad <anis@ficc.ai>
2021-12-07 14:31:13 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ficc',
+    version='0.1',
+    description='Ficc.ai internal library',
+    author='Ficc.ai',
+    author_email='anis@ficc.ai'
+    packages=find_packages()
+)
+
