2ceddae1ddfb60345ad5bdefc789477efe2648d6
Anis Ahmad <anis@ficc.ai>
2021-12-07 14:43:29 -0800

    Empty skeleton, add GitHub's gitignore for python projects (https://github.com/github/gitignore)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1',
     description='Ficc.ai internal library',
     author='Ficc.ai',
-    author_email='anis@ficc.ai'
+    author_email='anis@ficc.ai',
     packages=find_packages()
 )
 
