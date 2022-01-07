58350078156846effc014a34a653b4afe618648f
ahmad-ficc <ahmad@ficc.ai>
2022-01-07 13:49:41 -0800

    Standalone functionality to the get_ficc_ycl function

--- ficc/utils/yield_curve.py
+++ ficc/utils/yield_curve.py
@@ -2,7 +2,7 @@
  # @ Author: Ahmad Shayaan
  # @ Create Time: 2021-12-15 13:59:54
  # @ Modified by: Ahmad Shayaan
- # @ Modified time: 2021-12-17 11:14:30
+ # @ Modified time: 2022-01-07 13:48:08
  # @ Description: This file contains the code to get 
  # the ficc yield curve level using the calc_date
  '''
@@ -10,11 +10,12 @@
 
 import pandas as pd
 from datetime import datetime
-
+import sys
 from ficc.utils.nelson_seigel_model import yield_curve_level
+from ficc.utils.yield_curve_params import yield_curve_params
 import ficc.utils.globals as globals
 
-def get_ficc_ycl(trade):
+def get_ficc_ycl(trade, **kwargs):
     target_date = None
 
     # We only have reliable data to estimate the yield curve after
@@ -24,7 +25,20 @@ def get_ficc_ycl(trade):
     else:
         target_date = trade.trade_date
     duration = (trade.calc_date - target_date).days/365.25
-    ficc_yl = yield_curve_level(duration,
+    
+    try:
+        ficc_yl = yield_curve_level(duration,
+                                target_date.strftime('%Y-%m-%d'),
+                                globals.nelson_params,
+                                globals.scalar_params)
+    except Exception as e:
+        if 'client' not in kwargs:
+            raise Exception("Need to provied bigquery client if being used as a stand alone function")
+            sys.exit(0)
+        
+        bq_client = kwargs['client']
+        yield_curve_params(bq_client)
+        ficc_yl = yield_curve_level(duration,
                                 target_date.strftime('%Y-%m-%d'),
                                 globals.nelson_params,
                                 globals.scalar_params)
