abb2be6f2611260369e0f42dea72bbdf92dd2ae9
Anis Ahmad <a3ahmad@gmail.com>
2022-01-10 22:33:33 -0800

    Print results, if wandb is set to None, when evaluating a model

--- ficc/utils/eval.py
+++ ficc/utils/eval.py
@@ -12,7 +12,10 @@ def eval_model(
     y_test = create_labels(df)
 
     _, mae = model.evaluate(x_test, y_test, verbose=1)
-    wandb.log({"Full MAE": mae})
+    if wandb is not None:
+        wandb.log({"Full MAE": mae})
+    else:
+        print(f"Full MAE: {round(mae, 3)}")
 
     if categories is not None:
         for category, category_df in df.groupby(by=categories, squeeze=True):
@@ -23,4 +26,7 @@ def eval_model(
             if isinstance(category, tuple):
                 category = ','.join(category)
             
-            wandb.log({f"{category} MAE": mae})
+            if wandb is not None:
+                wandb.log({f"{category} MAE": mae})
+            else:
+                print(f"{category} MAE: {round(mae, 3)}")
