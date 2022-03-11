27098c3ceb44a65a763c00f705fe56840140f578
ahmad-ficc <ahmad@ficc.ai>
2022-03-11 10:41:11 -0800

    Fixes

--- ficc/utils/process_features.py
+++ ficc/utils/process_features.py
@@ -2,7 +2,7 @@
  # @ Author: Ahmad Shayaan
  # @ Create Time: 2021-12-17 12:09:34
  # @ Modified by: Ahmad Shayaan
- # @ Modified time: 2022-03-11 10:08:15
+ # @ Modified time: 2022-03-11 10:40:16
  # @ Description:
  '''
 import numpy as np
@@ -46,13 +46,13 @@ def process_features(df):
     df.loc[:, 'days_to_maturity'] =  np.log10(1 + (df.maturity_date - df.settlement_date).dt.days).fillna(0)
     df.loc[:, 'days_to_call'] = np.log10(1 + (df.next_call_date - df.settlement_date).dt.days).fillna(0)
     df.days_to_call.replace(-np.inf, np.nan, inplace=True)
-    df.days_to_call.fillna(0)
+    df.days_to_call.fillna(0, inplace=True)
     df.loc[:, 'days_to_refund'] = np.log10(1 + (df.refund_date - df.settlement_date).dt.days).fillna(0)
     
     df.loc[:, 'days_to_par'] = np.log10(1 + (df.par_call_date - df.settlement_date).dt.days).fillna(0)
     df.days_to_par.replace(-np.inf, np.nan, inplace=True)
-    df.days_to_par.fillna(0)
-    
+    df.days_to_par.fillna(0, inplace=True)
+
     df.loc[:, 'call_to_maturity'] = np.log10(1 + (df.maturity_date - df.next_call_date).dt.days).fillna(0)
     
     # Adding features of the last trade i.e the trade before the most recent trade
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ficc',
-    version='0.2.4',
+    version='0.2.5',
     description='Ficc.ai internal library',
     author='Ficc.ai',
     author_email='anis@ficc.ai',
