2bef55e77b6c765e618d8217eeb47ec2e315c26b
ahmad-ficc <ahmad@ficc.ai>
2022-03-23 11:14:57 -0700

    Large number fix

--- ficc/utils/days_in_interest_payment.py
+++ ficc/utils/days_in_interest_payment.py
@@ -2,7 +2,7 @@
  # @ Author: Ahmad Shayaan
  # @ Create Time: 2021-12-20 10:45:07
  # @ Modified by: Ahmad Shayaan
- # @ Modified time: 2021-12-20 10:56:33
+ # @ Modified time: 2022-03-23 10:56:59
  # @ Description: This file implements the function that calculates
  #  the number of days in the interest payment period
  '''
@@ -12,5 +12,7 @@ from ficc.utils.auxiliary_variables import COUPON_FREQUENCY_TYPE, LARGE_NUMBER
 def days_in_interest_payment(trade):
     frequency = COUPON_FREQUENCY_TYPE[trade['interest_payment_frequency']]
     if frequency == 0:
-        frequency = LARGE_NUMBER
+        # Changing it to large number from frequency = LARGE_NUMBER
+        return LARGE_NUMBER
+        
     return 360 / frequency
