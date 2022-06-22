faddfc95960a81ad5f1674053a00bebaf31e4f6a
Anis Ahmad <anis@ficc.ai>
2022-06-22 14:11:27 -0700

    Add centralized place to apply and test for exclusions.

---
+++ ficc/utils/exclusions.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+
+def apply_exclusions(df):
+    df = df[(df.days_to_call == 0) | (df.days_to_call > np.log10(400))]
+    df = df[(df.days_to_refund == 0) | (df.days_to_refund > np.log10(400))]
+    df = df[df.sinking == False]
+    df = df[df.incorporated_state_code != 'VI']
+    df = df[df.incorporated_state_code != 'GU']
+    df = df[df.coupon_type == 8]
+    df = df[df.is_called == False]
+
+    df = df[~df.purpose_sub_class.isin([6, 20, 21, 22, 44, 57, 90, 106])]
+    df = df[~df.called_redemption_type.isin([18, 19])]
+
+# TODO: We should have this function take in a potential trade, test to see if it would have been excluded. If not,
+# return None, else return a message explaining why.
+def test_for_exclusion():
+    pass
