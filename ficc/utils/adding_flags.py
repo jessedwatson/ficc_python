9b96764cc70c469045e2df5a54144f74f3bf3c34
Mitas Ray <mitas.ray@gmail.com>
2022-08-25 14:00:37 -0700

    Potentially faster implementation of adding NTBC flag

--- ficc/utils/adding_flags.py
+++ ficc/utils/adding_flags.py
@@ -191,13 +191,13 @@ def add_ntbc_precursor_flag(df, flag_name=NTBC_PRECURSOR, return_candidates_dict
     df[TRADE_DATETIME_DATE] = df['trade_datetime'].dt.date
 
     if return_candidates_dict: multiple_candidates = dict()    # initialize the dictionary
-
-    # for each NTBC customer trade, mark the closest inter-dealer trade on that day with the same quantity, price, and cusip
+    
+    dd = df[df['trade_type'] == 'D']    # any NTBC precursor candidate must be an inter-dealer trade
+    # for each NTBC customer trade, the inter-dealer trade must be on that day with the same quantity, price, and cusip
     features_to_match = ['cusip', 'quantity', 'dollar_price', TRADE_DATETIME_DATE]
-    condition_based_on_features_to_match = ' & '.join([f'(df["{feature}"] == ntbc_trade["{feature}"])' for feature in features_to_match])
+    ntbc_precursor_candidates_groups = dd.group_by(features_to_match)
     for _, ntbc_trade in df[df['is_non_transaction_based_compensation'] & ((df['trade_type'] == 'S') | (df['trade_type'] == 'P'))].iterrows():    # need the `ntbc_trade` variable name when evaluating `condition_based_on_features_to_match`
-        ntbc_precursor_candidates = df[eval(condition_based_on_features_to_match)]
-        ntbc_precursor_candidates = ntbc_precursor_candidates[ntbc_precursor_candidates['trade_type'] == 'D']    # candidates must be inter-dealer trades
+        ntbc_precursor_candidates = ntbc_precursor_candidates_groups.get_group(tuple([ntbc_trade[feature] for feature in features_to_match]))    # group header must be immutable, hence the tuple
         if return_candidates_dict and len(ntbc_precursor_candidates) != 1: 
             # print(f'{len(ntbc_precursor_candidates)} candidates found for rtrs control number: {ntbc_trade["rtrs_control_number"]}')
             num_ntbc_precursor_candidates = len(ntbc_precursor_candidates)
