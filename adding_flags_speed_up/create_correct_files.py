dbaa70cb4ac5b837d5c3e0cccd1870b3202454b7
Mitas Ray <mitas.ray@gmail.com>
2022-08-31 15:33:03 -0700

    Add comparison to ground truth

--- adding_flags_speed_up/create_correct_files.py
+++ adding_flags_speed_up/create_correct_files.py
@@ -1,6 +1,11 @@
+import os
 import time
+
 from datetime import datetime
 
+import pandas as pd
+
+from ficc.utils.auxiliary_variables import IS_BOOKKEEPING, IS_SAME_DAY, NTBC_PRECURSOR, IS_REPLICA
 from ficc.utils.adding_flags import add_bookkeeping_flag, add_same_day_flag, add_ntbc_precursor_flag, add_replica_flag
 from ficc.utils.adding_flags_v2 import add_bookkeeping_flag_v2, add_same_day_flag_v2, add_ntbc_precursor_flag_v2, add_replica_flag_v2
 
@@ -10,7 +15,10 @@ sys.path.insert(0, '/Users/mitas/ficc/ficc/ml_models/sequence_predictors/')
 from rating_model_mitas.data_prep import read_processed_file_pickle
 
 
-def add_flags(data, save_name=None):
+FLAGS = [IS_BOOKKEEPING, IS_SAME_DAY, NTBC_PRECURSOR, IS_REPLICA]
+
+
+def add_flags(data, save_filename=None):
     start_time = time.time()
     data = add_replica_flag(data)
     data = add_bookkeeping_flag(data)
@@ -19,11 +27,12 @@ def add_flags(data, save_name=None):
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f'Elapsed time: {elapsed_time} seconds')
-    if save_name != None: data.to_pickle(f'./files/{save_name}.pkl')
+    if save_filename != None: data.to_pickle(f'./files/{save_filename}.pkl')
     return data, elapsed_time
 
 
-def add_flags_v2(data, save_name=None):
+def add_flags_v2(data, save_filename=None, compare_filename=None):
+    if compare_filename != None: assert os.path.exists(f'./files/{compare_filename}.pkl'), 'No file to compare against'
     start_time = time.time()
     data = add_replica_flag_v2(data)
     data = add_bookkeeping_flag_v2(data)
@@ -32,15 +41,19 @@ def add_flags_v2(data, save_name=None):
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f'Elapsed time: {elapsed_time} seconds')
-    if save_name != None: data.to_pickle(f'./files/{save_name}_v2.pkl')
+    if save_filename != None: data.to_pickle(f'./files/{save_filename}_v2.pkl')
+    if compare_filename != None:
+        truth = pd.read_pickle(f'./files/{compare_filename}.pkl')
+        for flag in FLAGS:
+            assert data[flag].equals(truth[flag]), f'{flag} values are not equal'
     return data, elapsed_time
 
 
 if __name__ == '__main__':
     filename = '/Users/mitas/ficc/ficc/ml_models/sequence_predictors/data/processed_data_ficc_ycl_2021-12-31-23-59.pkl'
     three_months_data = read_processed_file_pickle(filename)
-    one_week_data = three_months_data[three_months_data['trade_datetime'] <= datetime.date(2021, 10, 8)]
-    one_month_data = three_months_data[three_months_data['trade_datetime'] <= datetime.date(2021, 10, 31)]
+    one_week_data = three_months_data[three_months_data['trade_datetime'] <= datetime(2021, 10, 8)]
+    one_month_data = three_months_data[three_months_data['trade_datetime'] <= datetime(2021, 10, 31)]
     _, elapsed_time_one_week = add_flags(one_week_data, 'one_week')
     _, elapsed_time_one_month = add_flags(one_month_data, 'one_month')
     _, elapsed_time_three_months = add_flags(three_months_data, 'three_months')
