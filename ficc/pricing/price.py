8545819b6db0749098a9c638b3724e6a42672515
ahmad-ficc <ahmad@ficc.ai>
2023-08-10 12:25:37 -0700

    Updated to pricing code from Charles

--- ficc/pricing/price.py
+++ ficc/pricing/price.py
@@ -1,18 +1,17 @@
 '''
  # @ Author: Mitas Ray
  # @ Create Time: 2022-01-13 23:04:00
- # @ Modified by: Mitas Ray
- # @ Modified time: 2022-02-25 14:04:00
+ # @ Modified by: Ahmad Shayaan
+ # @ Modified time: 2023-08-10 12:25:16
  # @ Description: This file implements functions to compute the price of a trade
  # given the yield.
  '''
-import numpy as np
 import pandas as pd
 
 from ficc.utils.auxiliary_variables import NUM_OF_DAYS_IN_YEAR
 from ficc.utils.auxiliary_functions import compare_dates
 from ficc.utils.diff_in_days import diff_in_days_two_dates
-from ficc.utils.frequency import get_time_delta_from_interest_frequency
+from ficc.utils.frequency import get_time_delta_from_interest_frequency, get_frequency
 from ficc.utils.truncation import trunc_and_round_price
 from ficc.pricing.auxiliary_functions import get_num_of_interest_payments_and_final_coupon_date, \
                                              price_of_bond_with_multiple_periodic_interest_payments, \
@@ -20,31 +19,7 @@ from ficc.pricing.auxiliary_functions import get_num_of_interest_payments_and_fi
                                              price_of_bond_with_interest_at_maturity
 from ficc.pricing.called_trade import end_date_for_called_bond, refund_price_for_called_bond
 
-'''
-This function is a helper function for `compute_price`. This function calculates the price of a trade, where `yield_rate` 
-is a specific yield and `end_date` is a fixed repayment date. All dates must be valid relative to the settlement 
-date, as opposed to the trade date. Note that "yield" is a reserved word in Python and should not be used as the name 
-of a variable or column.
 
-Formulas are from https://www.msrb.org/pdf.aspx?url=https%3A%2F%2Fwww.msrb.org%2FRules-and-Interpretations%2FMSRB-Rules%2FGeneral%2FRule-G-33.aspx.
-For all bonds, `base` is the present value of future cashflows to the buyer. 
-The clean price is this price minus the accumulated amount of simple interest that the buyer must pay to the seller, which is called `accrued`.
-Zero-coupon bonds are handled first. For these, the yield is assumed to be compounded semi-annually, i.e., once every six months.
-For bonds with non-zero coupon, the first and last interest payment periods may have a non-standard length,
-so they must be handled separately.
-
-When referring to the formulas in the MSRB handbook (link above), the below variables map to the code.
-A: prev_coupon_date_to_settlement_date
-B: NUM_OF_DAYS_IN_YEAR
-Y: yield_rate
-N: num_of_interest_payments
-E: num_of_days_in_period
-F: settlement_date_to_next_coupon_date
-P: price
-D: settlement_date_to_end_date
-H: prev_coupon_date_to_end_date
-R: coupon
-'''
 def get_price(cusip, 
               prev_coupon_date, 
               first_coupon_date, 
@@ -58,6 +33,29 @@ def get_price(cusip,
               RV, 
               time_delta, 
               last_period_accrues_from_date):
+    '''This function is a helper function for `compute_price`. This function calculates the price of a trade, where `yield_rate` 
+    is a specific yield and `end_date` is a fixed repayment date. All dates must be valid relative to the settlement 
+    date, as opposed to the trade date. Note that 'yield' is a reserved word in Python and should not be used as the name 
+    of a variable or column.
+
+    Formulas are from https://www.msrb.org/pdf.aspx?url=https%3A%2F%2Fwww.msrb.org%2FRules-and-Interpretations%2FMSRB-Rules%2FGeneral%2FRule-G-33.aspx.
+    For all bonds, `base` is the present value of future cashflows to the buyer. 
+    The clean price is this price minus the accumulated amount of simple interest that the buyer must pay to the seller, which is called `accrued`.
+    Zero-coupon bonds are handled first. For these, the yield is assumed to be compounded semi-annually, i.e., once every six months.
+    For bonds with non-zero coupon, the first and last interest payment periods may have a non-standard length,
+    so they must be handled separately.
+
+    When referring to the formulas in the MSRB handbook (link above), the below variables map to the code.
+    A: prev_coupon_date_to_settlement_date
+    B: NUM_OF_DAYS_IN_YEAR
+    Y: yield_rate
+    N: num_of_interest_payments
+    E: num_of_days_in_period
+    F: settlement_date_to_next_coupon_date
+    P: price
+    D: settlement_date_to_end_date
+    H: prev_coupon_date_to_end_date
+    R: coupon'''
     yield_rate = yield_rate / 100
     
     # Right now we do not disambiguate zero coupon from interest at maturity. More specfically, 
@@ -80,8 +78,11 @@ def get_price(cusip,
         num_of_days_in_period = NUM_OF_DAYS_IN_YEAR / frequency    # number of days in interest payment period 
         assert num_of_days_in_period == round(num_of_days_in_period)
          
-        if compare_dates(end_date, next_coupon_date) <= 0:
-            # MSRB Rule Book G-33, rule (b)(i)(B)(1)
+        if compare_dates(end_date, next_coupon_date) <= 0:    # MSRB Rule Book G-33, rule (b)(i)(B)(1)
+            if diff_in_days_two_dates(end_date, final_coupon_date) == 0:    # end date is the same (within one day) as the final coupon date, but the arithmetic later essentially includes one too many coupon payments; to fix this, one payment is subtracted from the redemption value (RV) and the final coupon date is shifted earlier
+                final_coupon_date = prev_coupon_date
+                RV = RV - coupon / frequency
+            
             settlement_date_to_end_date = diff_in_days_two_dates(end_date, settlement_date)
             final_coupon_date_to_end_date = diff_in_days_two_dates(end_date, final_coupon_date)
             interest_due_at_end_date = coupon * final_coupon_date_to_end_date / NUM_OF_DAYS_IN_YEAR
@@ -89,8 +90,7 @@ def get_price(cusip,
                    (1 + (yield_rate / frequency) * settlement_date_to_end_date / num_of_days_in_period)
             accrued = coupon * prev_coupon_date_to_settlement_date / NUM_OF_DAYS_IN_YEAR
             price = base - accrued
-        else:
-            # MSRB Rule Book G-33, rule (b)(i)(B)(2)
+        else:    # MSRB Rule Book G-33, rule (b)(i)(B)(2)
             price = price_of_bond_with_multiple_periodic_interest_payments(cusip, 
                                                                            settlement_date, 
                                                                            accrual_date, 
@@ -108,20 +108,16 @@ def get_price(cusip,
                                                                            last_period_accrues_from_date)              
     return trunc_and_round_price(price)
 
-'''
-This function computes the price of a trade. For bonds that have not been called, the price is the lowest of
-three present values: to the next call date (which may be above par), to the next par call date, and to maturity.
-Note that calling this function, we need to have: `data['interest_payment_frequency'] = data.interest_payment_frequency.map(COUPON_FREQUENCY_TYPE)`, 
-and it is important to note that this must be executed only ONCE since the column is being mutated, so multiple calls 
-of this function will lead to a column of NaN values.
-'''
+
 def compute_price(trade, yield_rate=None):
+    '''This function computes the price of a trade. For bonds that have not been called, the price is the lowest of
+    three present values: to the next call date (which may be above par), to the next par call date, and to maturity.'''
     if yield_rate == None:
-        yield_rate = trade['yield']
+        yield_rate = trade.ficc_ytw
     elif type(yield_rate) == str:
         raise ValueError('Yield rate argument cannot be a string. It must be a numerical value.')
 
-    frequency = trade.interest_payment_frequency
+    frequency = get_frequency(trade.interest_payment_frequency)
     time_delta = get_time_delta_from_interest_frequency(frequency)
     my_prev_coupon_date, my_next_coupon_date = get_prev_coupon_date_and_next_coupon_date(trade, frequency, time_delta)
 
@@ -139,30 +135,31 @@ def compute_price(trade, yield_rate=None):
                                                                     time_delta, 
                                                                     trade.last_period_accrues_from_date)
 
+    escrowed_to_maturity_but_callability_not_defeased = (trade.called_redemption_type in (1.0, 5.0)) and (trade.call_defeased != '1')    # explanation for this condition in section 'Bug Fix 4' of https://github.com/Ficc-ai/ficc/blob/ficc_ml/ml_models/sequence_predictors/yield_to_price_20230730.ipynb
     redemption_value_at_maturity = 100
-    if (not trade.is_called) and (not trade.is_callable):
+    if (not trade.is_called) and (not trade.is_callable) and (not escrowed_to_maturity_but_callability_not_defeased):
         yield_to_maturity = get_price_caller(trade.maturity_date, redemption_value_at_maturity)
         return yield_to_maturity, trade.maturity_date
     elif trade.is_called:
         end_date = end_date_for_called_bond(trade)
 
         if compare_dates(end_date, trade.settlement_date) < 0:
-            print(f"Bond (CUSIP: {trade.cusip}, RTRS: {trade.rtrs_control_number}) has an end date ({end_date}) which is after the settlement date ({trade.settlement_date}).")    # printing instead of raising an error to not disrupt processing large quantities of trades
-            # raise ValueError(f"Bond (CUSIP: {trade.cusip}, RTRS: {trade.rtrs_control_number}) has an end date ({end_date}) which is after the settlement date ({trade.settlement_date}).")
+            print(f'Bond (CUSIP: {trade.cusip}) has an end date ({end_date}) which is before the settlement date ({trade.settlement_date}).')    # printing instead of raising an error to not disrupt processing large quantities of trades; remove RTRS control number since new pipeline does not have it
+            # raise ValueError(f'Bond (CUSIP: {trade.cusip}, RTRS: {trade.rtrs_control_number}) has an end date ({end_date}) which is after the settlement date ({trade.settlement_date}).')
         
         redemption_value_at_refund = refund_price_for_called_bond(trade)
         return get_price_caller(end_date, redemption_value_at_refund), end_date
     else:
         next_price, to_par_price, maturity_price = float('inf'), float('inf'), float('inf')
 
-        if not pd.isnull(trade.par_call_date):
+        if not pd.isnull(trade.par_call_date) and trade.settlement_date < trade.par_call_date:
             to_par_price = get_price_caller(trade.par_call_date, trade.par_call_price)
-        if not pd.isnull(trade.next_call_date):
+        if not pd.isnull(trade.next_call_date) and trade.settlement_date < trade.next_call_date:
             next_price = get_price_caller(trade.next_call_date, trade.next_call_price)
         maturity_price = get_price_caller(trade.maturity_date, redemption_value_at_maturity)
 
         prices_and_dates = [(next_price, trade.next_call_date), 
                             (to_par_price, trade.par_call_date), 
                             (maturity_price, trade.maturity_date)]
-        calc_price, calc_date = min(prices_and_dates, key=lambda pair: pair[0])    # this function is stable and will choose the pair which appears first in the case of ties for the lowest price
-    return calc_price, calc_date
+        calc_price, calc_date = min(prices_and_dates, key=lambda pair: pair[0])    # the `min` function is stable and will choose the pair which appears first in the case of ties for the lowest price
+        return calc_price, calc_date
--- ficc/pricing/yield_rate.py
+++ ficc/pricing/yield_rate.py
@@ -1,8 +1,8 @@
 '''
  # @ Author: Mitas Ray
  # @ Create Time: 2022-01-13 23:20:00
- # @ Modified by: Mitas Ray
- # @ Modified time: 2022-02-25 14:04:00
+ # @ Modified by: Ahmad Shayaan
+ # @ Modified time: 2023-08-10 12:25:14
  # @ Description: This file implements functions to compute the yield of a trade
  # given the price.
  '''
@@ -12,7 +12,7 @@ import scipy.optimize as optimize
 from ficc.utils.auxiliary_variables import NUM_OF_DAYS_IN_YEAR
 from ficc.utils.auxiliary_functions import compare_dates
 from ficc.utils.diff_in_days import diff_in_days_two_dates
-from ficc.utils.frequency import get_time_delta_from_interest_frequency
+from ficc.utils.frequency import get_time_delta_from_interest_frequency, get_frequency
 from ficc.utils.truncation import trunc_and_round_yield
 from ficc.pricing.auxiliary_functions import get_num_of_interest_payments_and_final_coupon_date, \
                                              price_of_bond_with_multiple_periodic_interest_payments, \
@@ -20,17 +20,7 @@ from ficc.pricing.auxiliary_functions import get_num_of_interest_payments_and_fi
                                              price_of_bond_with_interest_at_maturity
 from ficc.pricing.called_trade import end_date_for_called_bond, refund_price_for_called_bond
 
-'''
-This function is a helper function for `compute_yield`. This function calculates the yield of a trade given the price and other trade features, using
-the MSRB Rule Book G-33 linked at: https://www.msrb.org/pdf.aspx?url=https%3A%2F%2Fwww.msrb.org%2FRules-and-Interpretations%2FMSRB-Rules%2FGeneral%2FRule-G-33.aspx.
 
-When referring to the formulas in the MSRB handbook (link above), the below variables map to the code.
-B: NUM_OF_DAYS_IN_YEAR
-E: number of days in interest payment period
-M: frequency
-N: num_of_interest_payments
-R: coupon
-'''
 def get_yield(cusip, 
               prev_coupon_date, 
               first_coupon_date, 
@@ -44,6 +34,15 @@ def get_yield(cusip,
               RV, 
               time_delta, 
               last_period_accrues_from_date):
+    '''This function is a helper function for `compute_yield`. This function calculates the yield of a trade given the price and other trade features, using
+    the MSRB Rule Book G-33 linked at: https://www.msrb.org/pdf.aspx?url=https%3A%2F%2Fwww.msrb.org%2FRules-and-Interpretations%2FMSRB-Rules%2FGeneral%2FRule-G-33.aspx.
+
+    When referring to the formulas in the MSRB handbook (link above), the below variables map to the code.
+    B: NUM_OF_DAYS_IN_YEAR
+    E: number of days in interest payment period
+    M: frequency
+    N: num_of_interest_payments
+    R: coupon'''
     settlement_date_to_end_date = diff_in_days_two_dates(end_date, settlement_date)    # hold period days
 
     if frequency == 0:
@@ -62,7 +61,6 @@ def get_yield(cusip,
                                                                                                          time_delta)
         prev_coupon_date_to_settlement_date = diff_in_days_two_dates(settlement_date, prev_coupon_date)    # accrued days from beginning of the interest payment period, used to be labelled `A`
         prev_coupon_date_to_end_date = diff_in_days_two_dates(end_date, prev_coupon_date)    # accrual days for final paid coupon
-
         num_of_days_in_period = NUM_OF_DAYS_IN_YEAR / frequency    # number of days in interest payment period 
         assert num_of_days_in_period == round(num_of_days_in_period)
 
@@ -92,16 +90,15 @@ def get_yield(cusip,
             yield_estimate = optimize.newton(yield_func, x0=0, maxiter=int(1e3))
     return trunc_and_round_yield(yield_estimate * 100)
 
-'''
-This function computes the yield of a trade.
-'''
+
 def compute_yield(trade, price=None):
+    '''This function computes the yield of a trade.'''
     if price == None:
         price = trade.dollar_price
     elif type(price) == str:
         raise ValueError('Price argument cannot be a string. It must be a numerical value.')
 
-    frequency = trade.interest_payment_frequency
+    frequency = get_frequency(trade.interest_payment_frequency)
     time_delta = get_time_delta_from_interest_frequency(frequency)
     my_prev_coupon_date, my_next_coupon_date = get_prev_coupon_date_and_next_coupon_date(trade, frequency, time_delta)
 
@@ -112,23 +109,24 @@ def compute_yield(trade, price=None):
                                                                     end_date, 
                                                                     trade.settlement_date, 
                                                                     trade.accrual_date, 
-                                                                    trade.interest_payment_frequency,
+                                                                    frequency,
                                                                     price, 
                                                                     trade.coupon, 
                                                                     redemption_value, 
                                                                     time_delta, 
                                                                     trade.last_period_accrues_from_date)
 
+    escrowed_to_maturity_but_callability_not_defeased = (trade.called_redemption_type in (1.0, 5.0)) and (trade.call_defeased != '1')    # explanation for this condition in section 'Bug Fix 4' of https://github.com/Ficc-ai/ficc/blob/ficc_ml/ml_models/sequence_predictors/yield_to_price_20230730.ipynb
     redemption_value_at_maturity = 100
-    if (not trade.is_called) and (not trade.is_callable):
+    if (not trade.is_called) and (not trade.is_callable) and (not escrowed_to_maturity_but_callability_not_defeased):
         yield_to_maturity = get_yield_caller(trade.maturity_date, redemption_value_at_maturity)
         return yield_to_maturity, trade.maturity_date
     elif trade.is_called:
         end_date = end_date_for_called_bond(trade)
 
         if compare_dates(end_date, trade.settlement_date) < 0:
-            print(f"Bond (CUSIP: {trade.cusip}, RTRS: {trade.rtrs_control_number}) has an end date ({end_date}) which is after the settlement date ({trade.settlement_date}).")    # printing instead of raising an error to not disrupt processing large quantities of trades
-            # raise ValueError(f"Bond (CUSIP: {trade.cusip}, RTRS: {trade.rtrs_control_number}) has an end date ({end_date}) which is after the settlement date ({trade.settlement_date}).")
+            print(f'Bond (CUSIP: {trade.cusip}) has an end date ({end_date}) which is before the settlement date ({trade.settlement_date}).')    # printing instead of raising an error to not disrupt processing large quantities of trades
+            # raise ValueError(f'Bond (CUSIP: {trade.cusip}, RTRS: {trade.rtrs_control_number}) has an end date ({end_date}) which is after the settlement date ({trade.settlement_date}).')
 
         redemption_value_at_refund = refund_price_for_called_bond(trade)
         yield_to_call = get_yield_caller(end_date, redemption_value_at_refund)
@@ -136,14 +134,14 @@ def compute_yield(trade, price=None):
     else:
         yield_to_par_call, yield_to_next_call, yield_to_maturity = float('inf'), float('inf'), float('inf')
         
-        if not pd.isnull(trade.par_call_date):
+        if not pd.isnull(trade.par_call_date) and trade.settlement_date < trade.par_call_date:
             yield_to_par_call = get_yield_caller(trade.par_call_date, trade.par_call_price)
-        if not pd.isnull(trade.next_call_date):
+        if not pd.isnull(trade.next_call_date) and trade.settlement_date < trade.next_call_date:
             yield_to_next_call = get_yield_caller(trade.next_call_date, trade.next_call_price)
         yield_to_maturity = get_yield_caller(trade.maturity_date, redemption_value_at_maturity)
 
         yield_rates_and_dates = [(yield_to_next_call, trade.next_call_date), 
                                  (yield_to_par_call, trade.par_call_date), 
                                  (yield_to_maturity, trade.maturity_date)]
-        yield_to_worst, calc_date = min(yield_rates_and_dates, key=lambda pair: pair[0])    # this function is stable and will choose the pair which appears first in the case of ties for the lowest yield
+        yield_to_worst, calc_date = min(yield_rates_and_dates, key=lambda pair: pair[0])    # the `min` function is stable and will choose the pair which appears first in the case of ties for the lowest yield
         return yield_to_worst, calc_date
