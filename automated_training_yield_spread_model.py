c68f3cf861d9cb3652b1f0d39c7e9fabe2d7154e
Mitas Ray <mitas.ray@gmail.com>
2024-02-19 11:16:07 -0800

    Print dataset name when applying exclusions

--- automated_training_yield_spread_model.py
+++ automated_training_yield_spread_model.py
@@ -2,7 +2,7 @@
  # @ Author: Ahmad Shayaan
  # @ Create date: 2023-01-23
  # @ Modified by: Mitas Ray
- # @ Modified date: 2024-02-16
+ # @ Modified date: 2024-02-19
  '''
 import numpy as np
 import pandas as pd
@@ -139,33 +139,34 @@ def segment_results(data: pd.DataFrame):
     return result_df
 
 
-def apply_exclusions(data: pd.DataFrame):
+def apply_exclusions(data: pd.DataFrame, dataset_name: str = None):
+    from_dataset_name = f' from {dataset_name}' if dataset_name is not None else ''
     data_before_exclusions = data[:]
     previous_size = len(data)
 
     data = data[(data.days_to_call == 0) | (data.days_to_call > np.log10(400))]
     current_size = len(data)
-    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades for having 0 < days_to_call <= 400')
+    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades{from_dataset_name} for having 0 < days_to_call <= 400')
     previous_size = current_size
 
     data = data[(data.days_to_refund == 0) | (data.days_to_refund > np.log10(400))]
     current_size = len(data)
-    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades for having 0 < days_to_refund <= 400')
+    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades{from_dataset_name} for having 0 < days_to_refund <= 400')
     previous_size = current_size
 
     data = data[(data.days_to_maturity == 0) | (data.days_to_maturity > np.log10(400))]
     current_size = len(data)
-    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades for having 0 < days_to_maturity <= 400')
+    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades{from_dataset_name} for having 0 < days_to_maturity <= 400')
     previous_size = current_size
 
     data = data[data.days_to_maturity < np.log10(30000)]
     current_size = len(data)
-    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades for having days_to_maturity >= 30000')
+    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades{from_dataset_name} for having days_to_maturity >= 30000')
     previous_size = current_size
 
     data = data[~data.last_calc_date.isna()]
     current_size = len(data)
-    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades for having a null value for last_calc_date')
+    if previous_size != current_size: print(f'Removed {previous_size - current_size} trades{from_dataset_name} for having a null value for last_calc_date')
 
     return data, data_before_exclusions
 
@@ -177,7 +178,7 @@ def train_model(data: pd.DataFrame, last_trade_date, num_features_for_each_trade
 
     if TESTING: last_trade_date = get_trade_date_where_data_exists_after_this_date(last_trade_date, data, exclusions_function=apply_exclusions)
     test_data = data[data.trade_date > last_trade_date]
-    test_data, test_data_before_exclusions = apply_exclusions(test_data)
+    test_data, test_data_before_exclusions = apply_exclusions(test_data, 'test_data')
     if len(test_data) == 0:
         print(f'No model is trained since there are no trades in `test_data`; `train_model(...)` is terminated')
         return None, None, None
