b156caca0a8ce0450c41a5bac28730cbe20a5da5
Mitas Ray <mitas.ray@gmail.com>
2024-04-15 15:56:36 -0700

    Create a yield spread model with similar trades deployment script

--- yield_spread_deployment.sh
+++ yield_spread_deployment.sh
@@ -1,7 +1,7 @@
 # @ Author: Ahmad Shayaan
 # @ Create date: 2023-07-28
 # @ Modified by: Mitas Ray
-# @ Modified date: 2023-04-10
+# @ Modified date: 2024-04-10
 echo "If there are errors, visit: https://www.notion.so/Daily-Model-Deployment-Process-d055c30e3c954d66b888015226cbd1a8"
 echo "Search for warnings in the logs (even on a successful training procedure) and investigate"
 
