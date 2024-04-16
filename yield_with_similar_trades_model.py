aa6f7608acb318e85e331c4d43fcc56c49437552
Mitas Ray <mitas.ray@gmail.com>
2024-04-16 15:14:12 -0700

    Create yield spread with similar trades model

--- yield_with_similar_trades_model.py
+++ yield_with_similar_trades_model.py
@@ -42,27 +42,39 @@ class CustomAttention(tf.keras.layers.Layer):
         return output    
 
 
-def model_definition(trade_history_normalizer,
-                     noncat_binary_normalizer,
+def model_definition(trade_history_normalizer, 
+                     similar_trade_history_normalizer, 
+                     noncat_binary_normalizer, 
                      num_trades_in_history, 
                      num_features_for_each_trade_in_history, 
                      categorical_features, 
                      non_cat_features, 
-                     binary_features,
+                     binary_features, 
                      fmax):
     inputs = []
     layer = []
 
     ############## INPUT BLOCK ###################
+    
+    similar_trade_history_input = layers.Input(name='similar_trade_history_input', 
+                                               shape=(num_trades_in_history, num_features_for_each_trade_in_history), 
+                                               dtype=tf.float32)
+    
+    target_attention_input_for_similar_trade_history = layers.Input(name='target_attention_input_for_similar_trade_history', 
+                                                                    shape=(1, 3), 
+                                                                    dtype=tf.float32) 
+    
     trade_history_input = layers.Input(name='trade_history_input', 
                                        shape=(num_trades_in_history, num_features_for_each_trade_in_history), 
-                                       dtype=tf.float32) 
+                                       dtype=tf.float32)
 
     target_attention_input = layers.Input(name='target_attention_input', 
                                           shape=(1, 3), 
-                                          dtype=tf.float32) 
+                                          dtype=tf.float32)
 
 
+    inputs.append(similar_trade_history_input)
+    inputs.append(target_attention_input_for_similar_trade_history)
     inputs.append(trade_history_input)
     inputs.append(target_attention_input)
 
@@ -72,7 +84,41 @@ def model_definition(trade_history_normalizer,
     ))
 
 
-    layer.append(noncat_binary_normalizer(inputs[2]))
+    layer.append(noncat_binary_normalizer(inputs[-1]))
+
+    ####################################################
+
+
+    ############## SIMILAR TRADE HISTORY MODEL #################
+
+    lstm_layer_similar_trades = layers.Bidirectional(layers.LSTM(50, 
+                                                     activation='tanh',
+                                                     input_shape=(num_trades_in_history, num_features_for_each_trade_in_history),
+                                                     return_sequences=True,
+                                                     name='LSTM_similar_trades'))
+
+    lstm_layer_similar_trades_2 = layers.Bidirectional(layers.LSTM(100, 
+                                                       activation='tanh',
+                                                       input_shape=(num_trades_in_history, 50),
+                                                       return_sequences=True,
+                                                       name='LSTM_2_similar_trades'))
+
+
+
+    similar_trade_history_features = lstm_layer_similar_trades(similar_trade_history_normalizer(inputs[0]))
+    similar_trade_history_features = lstm_layer_similar_trades_2(similar_trade_history_features)
+
+
+    similar_trade_history_attention_sequence = layers.Dense(200, activation='relu', name='similar_trade_history_attention_dense')(target_attention_input)
+    similar_trade_history_attention = layers.Dot(axes=[2, 2])([similar_trade_history_features, similar_trade_history_attention_sequence])
+    similar_trade_history_attention = layers.Activation('softmax')(similar_trade_history_attention)
+
+    similar_trade_history_context_vector = layers.Dot(axes=[1, 1])([similar_trade_history_features, similar_trade_history_attention])
+    similar_trade_history_context_vector = layers.Flatten(name='similar_trade_history_context_vector_flatten')(similar_trade_history_context_vector)
+
+    similar_trade_history_context_vector = layers.BatchNormalization()(similar_trade_history_context_vector)
+    similar_trade_history_output = layers.Dense(100, activation='relu')(similar_trade_history_context_vector)
+
     ####################################################
 
 
@@ -92,7 +138,7 @@ def model_definition(trade_history_normalizer,
 
 
 
-    features = lstm_layer(trade_history_normalizer(inputs[0]))
+    features = lstm_layer(trade_history_normalizer(inputs[2]))
     features = lstm_layer_2(features)
 
 
@@ -104,7 +150,7 @@ def model_definition(trade_history_normalizer,
     context_vector = layers.Flatten(name='context_vector_flatten')(context_vector)
 
     context_vector = layers.BatchNormalization()(context_vector)
-    trade_history_output = layers.Dense(100,activation='relu')(context_vector)
+    trade_history_output = layers.Dense(100, activation='relu')(context_vector)
 
     ####################################################
 
@@ -133,7 +179,7 @@ def model_definition(trade_history_normalizer,
 
     ####################################################
 
-    feed_forward_input = layers.concatenate([reference_output, trade_history_output])
+    feed_forward_input = layers.concatenate([reference_output, trade_history_output, similar_trade_history_output])
 
     hidden = layers.Dense(300, activation='relu')(feed_forward_input)
     hidden = layers.BatchNormalization()(hidden)
@@ -149,21 +195,25 @@ def model_definition(trade_history_normalizer,
     return model
 
 
-def yield_spread_model(x_train, 
-                       num_trades_in_history, 
-                       num_features_for_each_trade_in_history, 
-                       categorical_features, 
-                       non_cat_features, 
-                       binary_features,
-                       fmax):
+def yield_spread_with_similar_trades_model(x_train, 
+                                           num_trades_in_history, 
+                                           num_features_for_each_trade_in_history, 
+                                           categorical_features, 
+                                           non_cat_features, 
+                                           binary_features,
+                                           fmax):
+    
+    similar_trade_history_normalizer = Normalization(name='Similar_trade_history_normalizer')
+    similar_trade_history_normalizer.adapt(x_train[0], batch_size=BATCH_SIZE)
     
     trade_history_normalizer = Normalization(name='Trade_history_normalizer')
-    trade_history_normalizer.adapt(x_train[0], batch_size=BATCH_SIZE)
+    trade_history_normalizer.adapt(x_train[1], batch_size=BATCH_SIZE)
 
     noncat_binary_normalizer = Normalization(name='Numerical_binary_normalizer')
-    noncat_binary_normalizer.adapt(x_train[2], batch_size=BATCH_SIZE)
+    noncat_binary_normalizer.adapt(x_train[-1], batch_size=BATCH_SIZE)
 
-    model = model_definition(trade_history_normalizer,
+    model = model_definition(trade_history_normalizer, 
+                             similar_trade_history_normalizer, 
                              noncat_binary_normalizer, 
                              num_trades_in_history, 
                              num_features_for_each_trade_in_history, 
