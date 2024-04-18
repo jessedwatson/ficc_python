b67f4f3380586bd64c5d37d9d2e349a9b53a4a33
mitasray <mitas.ray@gmail.com>
2024-04-18 07:27:22 +0000

    Change earliest trade datetime when testing to be april 1

--- automated_training_auxiliary_variables.py
+++ automated_training_auxiliary_variables.py
@@ -179,7 +179,7 @@ if TESTING:
     SAVE_MODEL_AND_DATA = False
     USE_PICKLED_DATA = True
     NUM_EPOCHS = 2
-    EARLIEST_TRADE_DATETIME = '2024-03-01T00:00:00'
+    EARLIEST_TRADE_DATETIME = '2024-04-01T00:00:00'
     print(f'In TESTING mode; SAVE_MODEL_AND_DATA=False and NUM_EPOCHS={NUM_EPOCHS} and EARLIEST_TRADE_DATETIME={EARLIEST_TRADE_DATETIME}')
     print('Check `get_creds(...)` to make sure the credentials filepath is correct')
     print('Check `WORKING_DIRECTORY` to make sure the path is correct')
