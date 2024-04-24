cce3282a7bafde6599f22eb13929d542c033952a
Mitas Ray <mitas.ray@gmail.com>
2024-04-24 18:37:24 +0000

    Add progress bar when not using multiprocessing

--- get_raw_data.py
+++ get_raw_data.py
@@ -2,12 +2,13 @@
  # @ Author: Mitas Ray
  # @ Create date: 2024-04-19
  # @ Modified by: Mitas Ray
- # @ Modified date: 2024-04-19
+ # @ Modified date: 2024-04-23
  # @ Description: Gather train / test data from materialized trade history. First, find all dates for which there are trades. Then, 
  use multiprocessing to read the data from BigQuery for each date, since the conversion of the query results to a dataframe is costly. 
  This file was created to test different ways of getting the raw data to determine which one was faster: getting it all at once, or 
  getting it day by day using multiprocessing and then concatenating it together.
  '''
+from tqdm import tqdm
 import multiprocess as mp
 from datetime import datetime
 import pandas as pd
@@ -53,15 +54,15 @@ def get_trades_for_all_dates(dates: list, all_at_once: bool = False) -> pd.DataF
         trades_for_all_dates = get_trades_for_particular_date(min(dates), max(dates))
     else:
         if len(dates) > 1 and MULTIPROCESSING:
-            print(f'Using multiprocessing for calling `get_trades_for_particular_date(...) on each item in {dates}')
-            with mp.Pool() as pool_object:    # using template from https://docs.python.org/3/library/multiprocessing.html
+            print(f'Using multiprocessing for calling `get_trades_for_particular_date(...) on each of the {len(dates)} items in {dates}')
+            with mp.Pool() as pool_object:    # using template from https://docs.python.org/3/library/multiprocessing.html; consider trying with lesser processes if running out of RAM (put an argument of e.g. `os.cpu_count() // 2` as the argument to `mp.Pool()` so it reads `mp.Pool(os.cpu_count() // 2)`)
                 trades_for_all_dates = pool_object.map(get_trades_for_particular_date, dates)
         else:
-            trades_for_all_dates = [get_trades_for_particular_date(date) for date in dates]
+            trades_for_all_dates = [get_trades_for_particular_date(date) for date in tqdm(dates)]
         trades_for_all_dates = pd.concat(trades_for_all_dates).reset_index(drop=True)    # `pd.concat(...)` is a bottleneck (even though the calls to `get_trades_for_particular_date(...)` take less than a minute, the `pd.concat(...)` takes >10 mins
     return trades_for_all_dates
 
 
-trades_for_all_dates = get_trades_for_all_dates(distinct_dates, True)
+trades_for_all_dates = get_trades_for_all_dates(distinct_dates, False)
 trades_for_all_dates.to_pickle('files/trades_for_all_dates_from_get_raw_data.pkl')
 print(trades_for_all_dates)
