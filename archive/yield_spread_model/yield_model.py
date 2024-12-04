f432c73e03cecda18a40be4ce310ad1c06292170
Mitas Ray <mitas.ray@gmail.com>
2024-12-03 17:29:03 -0800

    Remove old CustomAttention layer from model definition

--- archive/yield_spread_model/yield_model.py
+++ archive/yield_spread_model/yield_model.py
@@ -12,35 +12,10 @@ from tensorflow.keras.layers.experimental.preprocessing import Normalization
 
 from automated_training_auxiliary_variables import BATCH_SIZE, DROPOUT
 from set_random_seed import set_seed
+# from custom_attention import CustomAttention
 
 
 set_seed()
-
-
-class CustomAttention(tf.keras.layers.Layer):
-    def __init__(self, depth):
-        super(CustomAttention, self).__init__()
-        self.depth = depth
-        self.wq = layers.Dense(depth, name='weights_query') 
-        self.wk = layers.Dense(depth, name='weights_key')
-        self.wv = layers.Dense(depth, name='weights_value')
-
-    def scaled_dot_product_attention(self, q, v, k):
-        matmul_qk = tf.matmul(q, k, transpose_b=True)
-        scaling = tf.cast(tf.shape(k)[-1], tf.float32)
-        scaled_attention_logits = matmul_qk / tf.math.sqrt(scaling)
-        
-        attention_weights = tf.nn.softmax(scaled_attention_logits, axis=1) 
-        output = tf.matmul(attention_weights, v)
-        
-        return output
-    
-    def call(self, q, v, k):
-        q = self.wq(q)
-        v = self.wv(v)
-        k = self.wk(k)
-        output = self.scaled_dot_product_attention(q, v, k)
-        return output    
 
 
 def model_definition(trade_history_normalizer,
